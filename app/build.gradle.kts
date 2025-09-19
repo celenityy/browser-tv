352cee0f4a6f18e9aeb7d15c3a042df0e522730b
celenity <celenity@celenity.dev>
2025-09-18 23:10:23 -0400

    feat: Update androidx.lifecycle to 2.9.4
    Signed-off-by: celenity <celenity@celenity.dev>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -125,7 +125,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.10")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.4")
 
     val roomVersion = "2.6.1"
     implementation("androidx.room:room-runtime:$roomVersion")
