7c813501bf3fca05e4e30ff89ed2cfa486d91b82
celenity <celenity@celenity.dev>
2025-09-18 22:33:53 -0400

    feat: Update Gradle to 8.13
    Signed-off-by: celenity <celenity@celenity.dev>

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.2.2")
+        classpath("com.android.tools.build:gradle:8.13.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
         classpath("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:1.9.10-1.0.13")
 
