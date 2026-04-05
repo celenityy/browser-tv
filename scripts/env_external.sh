0566b8e6b0fc90086dcee8b61a9806ccf36364c9
celenity <celenity@celenity.dev>
2026-04-05 03:53:21 +0000

    fix: set external envs as `readonly`
    Signed-off-by: celenity <celenity@celenity.dev>

--- scripts/env_external.sh
+++ scripts/env_external.sh
@@ -9,152 +9,211 @@
 ## Use the Browser TV equivalent variables (at `env_common.sh`) instead.
 
 # Compiler flags
-export TARGET_CFLAGS="${BROWSER_TV_COMPILER_FLAGS}"
-export TARGET_CXXFLAGS="${BROWSER_TV_COMPILER_FLAGS}"
+readonly TARGET_CFLAGS="${BROWSER_TV_COMPILER_FLAGS}"
+readonly TARGET_CXXFLAGS="${BROWSER_TV_COMPILER_FLAGS}"
+export TARGET_CFLAGS
+export TARGET_CXXFLAGS
 
 # Gradle flags
-export GRADLE_FLAGS="${BROWSER_TV_GRADLE_FLAGS}"
+readonly GRADLE_FLAGS="${BROWSER_TV_GRADLE_FLAGS}"
+export GRADLE_FLAGS
 
 # Rust flags
-export CARGO_BUILD_RUSTDOCFLAGS="${BROWSER_TV_RUST_FLAGS}"
-export RUSTDOCFLAGS="${BROWSER_TV_RUST_FLAGS}"
+readonly CARGO_BUILD_RUSTDOCFLAGS="${BROWSER_TV_RUST_FLAGS}"
+readonly RUSTDOCFLAGS="${BROWSER_TV_RUST_FLAGS}"
+export CARGO_BUILD_RUSTDOCFLAGS
+export RUSTDOCFLAGS
 
 # Android SDK
-export ANDROID_HOME="${BROWSER_TV_ANDROID_SDK}"
-export ANDROID_SDK_ROOT="${BROWSER_TV_ANDROID_SDK}"
+readonly ANDROID_HOME="${BROWSER_TV_ANDROID_SDK}"
+readonly ANDROID_SDK_ROOT="${BROWSER_TV_ANDROID_SDK}"
+export ANDROID_HOME
+export ANDROID_SDK_ROOT
 
 # Android NDK
-export ANDROID_NDK_HOME="${BROWSER_TV_ANDROID_NDK}"
-export ANDROID_NDK_ROOT="${BROWSER_TV_ANDROID_NDK}"
+readonly ANDROID_NDK_HOME="${BROWSER_TV_ANDROID_NDK}"
+readonly ANDROID_NDK_ROOT="${BROWSER_TV_ANDROID_NDK}"
+export ANDROID_NDK_HOME
+export ANDROID_NDK_ROOT
 
 # Gradle cache
-export CACHEDIR="${BROWSER_TV_GRADLE_CACHE}"
+readonly CACHEDIR="${BROWSER_TV_GRADLE_CACHE}"
+export CACHEDIR
 
 # Gradle home
-export GRADLE_USER_HOME="${BROWSER_TV_GRADLE_HOME}"
+readonly GRADLE_USER_HOME="${BROWSER_TV_GRADLE_HOME}"
+export GRADLE_USER_HOME
 
 # Java home
-export JAVA_HOME="${BROWSER_TV_JAVA_HOME}"
+readonly JAVA_HOME="${BROWSER_TV_JAVA_HOME}"
+export JAVA_HOME
 
 # llvm-profdata
-export LLVM_PROFDATA="${BROWSER_TV_LLVM_PROFDATA}"
+readonly LLVM_PROFDATA="${BROWSER_TV_LLVM_PROFDATA}"
+export LLVM_PROFDATA
 
 # Mach
 ## https://firefox-source-docs.mozilla.org/mach/usage.html#user-settings
 ## https://searchfox.org/mozilla-central/rev/f008b9aa/python/mach/mach/telemetry.py#95
 ## https://searchfox.org/mozilla-central/rev/f008b9aa/python/mach/mach/telemetry.py#284
-export DISABLE_TELEMETRY=1
-export MACHRC="${BROWSER_TV_PATCHES}/machrc"
-export MOZCONFIG="${BROWSER_TV_GECKO}/mozconfig"
+readonly DISABLE_TELEMETRY=1
+readonly MACHRC="${BROWSER_TV_PATCHES}/machrc"
+readonly MOZCONFIG="${BROWSER_TV_GECKO}/mozconfig"
+export DISABLE_TELEMETRY
+export MACHRC
+export MOZCONFIG
 
 # microG
-export GRADLE_MICROG_VERSION_WITHOUT_GIT=1
+readonly GRADLE_MICROG_VERSION_WITHOUT_GIT=1
+export GRADLE_MICROG_VERSION_WITHOUT_GIT
 
 # mozbuild
-export MOZBUILD_STATE_PATH="${BROWSER_TV_MOZBUILD}"
+readonly MOZBUILD_STATE_PATH="${BROWSER_TV_MOZBUILD}"
+export MOZBUILD_STATE_PATH
 
 # No-op Taskcluster
 ## This should help ensure we don't fetch Mozilla artifacts/prebuilds
-export TASKCLUSTER_PROXY_URL='https://noop.invalid'
-export TASKCLUSTER_ROOT_URL='https://noop.invalid'
+readonly TASKCLUSTER_PROXY_URL='https://noop.invalid'
+readonly TASKCLUSTER_ROOT_URL='https://noop.invalid'
+export TASKCLUSTER_PROXY_URL
+export TASKCLUSTER_ROOT_URL
 
 # Phoenix
-export PHOENIX_ANDROID_ONLY=1
-export PHOENIX_CURL_FLAGS="${BROWSER_TV_CURL_FLAGS}"
-export PHOENIX_CURL_FLAGS_OVERRIDE=1
-export PHOENIX_EXTENDED_ONLY=1
-export PHOENIX_EXTRA_CFG=1
-export PHOENIX_EXTRA_CFG_FILE="${BROWSER_TV_BUILD}/tmp/gecko/browser-tv-parsed.cfg"
-export PHOENIX_EXTRA_CFG_OUTPUT_DIR="${BROWSER_TV_GECKO}/browser-tv/prefs"
-export PHOENIX_EXTRA_EXTENDED_OUTPUT_FILENAME_ANDROID='browser-tv'
-export PHOENIX_EXTRA_POLICIES_ANDROID=1
-export PHOENIX_EXTRA_POLICIES_FILE_ANDROID="${BROWSER_TV_PATCHES}/build/gecko/policies.json"
-export PHOENIX_EXTRA_POLICIES_OUTPUT_DIR_ANDROID="${BROWSER_TV_GECKO}/browser-tv/prefs"
-export PHOENIX_PYENV_DIR="${BROWSER_TV_PYENV_DIR}"
-export PHOENIX_PYTHON="${BROWSER_TV_PYTHON}"
-export PHOENIX_SED="${BROWSER_TV_SED}"
-export PHOENIX_TAR="${BROWSER_TV_TAR}"
-export PHOENIX_SPECS=0
+readonly PHOENIX_ANDROID_ONLY=1
+readonly PHOENIX_CURL_FLAGS="${BROWSER_TV_CURL_FLAGS}"
+readonly PHOENIX_CURL_FLAGS_OVERRIDE=1
+readonly PHOENIX_EXTENDED_ONLY=1
+readonly PHOENIX_EXTRA_CFG=1
+readonly PHOENIX_EXTRA_CFG_FILE="${BROWSER_TV_BUILD}/tmp/gecko/browser-tv-parsed.cfg"
+readonly PHOENIX_EXTRA_CFG_OUTPUT_DIR="${BROWSER_TV_GECKO}/browser-tv/prefs"
+readonly PHOENIX_EXTRA_EXTENDED_OUTPUT_FILENAME_ANDROID='browser-tv'
+readonly PHOENIX_EXTRA_POLICIES_ANDROID=1
+readonly PHOENIX_EXTRA_POLICIES_FILE_ANDROID="${BROWSER_TV_PATCHES}/build/gecko/policies.json"
+readonly PHOENIX_EXTRA_POLICIES_OUTPUT_DIR_ANDROID="${BROWSER_TV_GECKO}/browser-tv/prefs"
+readonly PHOENIX_PYENV_DIR="${BROWSER_TV_PYENV_DIR}"
+readonly PHOENIX_PYTHON="${BROWSER_TV_PYTHON}"
+readonly PHOENIX_SED="${BROWSER_TV_SED}"
+readonly PHOENIX_SPECS=0
+readonly PHOENIX_TAR="${BROWSER_TV_TAR}"
+export PHOENIX_ANDROID_ONLY
+export PHOENIX_CURL_FLAGS
+export PHOENIX_CURL_FLAGS_OVERRIDE
+export PHOENIX_EXTENDED_ONLY
+export PHOENIX_EXTRA_CFG
+export PHOENIX_EXTRA_CFG_FILE
+export PHOENIX_EXTRA_CFG_OUTPUT_DIR
+export PHOENIX_EXTRA_EXTENDED_OUTPUT_FILENAME_ANDROID
+export PHOENIX_EXTRA_POLICIES_ANDROID
+export PHOENIX_EXTRA_POLICIES_FILE_ANDROID
+export PHOENIX_EXTRA_POLICIES_OUTPUT_DIR_ANDROID
+export PHOENIX_PYENV_DIR
+export PHOENIX_PYTHON
+export PHOENIX_SED
+export PHOENIX_SPECS
+export PHOENIX_TAR
 
 # Python (rust-android-gradle)
 ## https://github.com/mozilla/rust-android-gradle?tab=readme-ov-file#specifying-paths-to-sub-commands-python-cargo-and-rustc
-export RUST_ANDROID_GRADLE_PYTHON_COMMAND="${BROWSER_TV_PYTHON}"
+readonly RUST_ANDROID_GRADLE_PYTHON_COMMAND="${BROWSER_TV_PYTHON}"
+export RUST_ANDROID_GRADLE_PYTHON_COMMAND
 
 # Rust (cargo)
-export CARGO="${BROWSER_TV_CARGO}"
-export CARGO_HOME="${BROWSER_TV_CARGO_HOME}"
-export CARGO_INSTALL_ROOT="${BROWSER_TV_CARGO_HOME}"
-export RUST_ANDROID_GRADLE_CARGO_COMMAND="${BROWSER_TV_CARGO}"
-export RUST_ANDROID_GRADLE_RUSTC_COMMAND="${BROWSER_TV_RUSTC}"
-export RUSTC="${BROWSER_TV_RUSTC}"
-export RUSTDOC="${BROWSER_TV_RUSTDOC}"
-
-## Always clean old files
-### https://doc.rust-lang.org/cargo/reference/config.html#cacheauto-clean-frequency
-export CARGO_CACHE_AUTO_CLEAN_FREQUENCY='always'
-
-## Disable caching compiler version information
-export CARGO_CACHE_RUSTC_INFO=0
+readonly CARGO="${BROWSER_TV_CARGO}"
+readonly CARGO_HOME="${BROWSER_TV_CARGO_HOME}"
+readonly CARGO_INSTALL_ROOT="${BROWSER_TV_CARGO_HOME}"
+readonly RUST_ANDROID_GRADLE_CARGO_COMMAND="${BROWSER_TV_CARGO}"
+readonly RUST_ANDROID_GRADLE_RUSTC_COMMAND="${BROWSER_TV_RUSTC}"
+readonly RUSTC="${BROWSER_TV_RUSTC}"
+readonly RUSTDOC="${BROWSER_TV_RUSTDOC}"
+export CARGO
+export CARGO_HOME
+export CARGO_INSTALL_ROOT
+export RUST_ANDROID_GRADLE_CARGO_COMMAND
+export RUST_ANDROID_GRADLE_RUSTC_COMMAND
+export RUSTC
+export RUSTDOC
 
 ## Disable debug
-export CARGO_PROFILE_DEV_DEBUG=false
-export CARGO_PROFILE_DEV_DEBUG_ASSERTIONS=false
-export CARGO_PROFILE_RELEASE_DEBUG=false
-export CARGO_PROFILE_RELEASE_DEBUG_ASSERTIONS=false
+readonly CARGO_PROFILE_DEV_DEBUG='false'
+readonly CARGO_PROFILE_DEV_DEBUG_ASSERTIONS='false'
+readonly CARGO_PROFILE_RELEASE_DEBUG='false'
+readonly CARGO_PROFILE_RELEASE_DEBUG_ASSERTIONS='false'
+export CARGO_PROFILE_DEV_DEBUG
+export CARGO_PROFILE_DEV_DEBUG_ASSERTIONS
+export CARGO_PROFILE_RELEASE_DEBUG
+export CARGO_PROFILE_RELEASE_DEBUG_ASSERTIONS
 
 ## Disable HTTP debugging
-export CARGO_HTTP_DEBUG=false
+readonly CARGO_HTTP_DEBUG='false'
+export CARGO_HTTP_DEBUG
 
 ## Disable incremental compilation
+### (Ensures builds are fresh)
 ### https://doc.rust-lang.org/cargo/reference/profiles.html#incremental
-export CARGO_INCREMENTAL=0
+readonly CARGO_BUILD_INCREMENTAL='false'
+readonly CARGO_INCREMENTAL=0
+export CARGO_BUILD_INCREMENTAL
+export CARGO_INCREMENTAL
 
 ## Display progress bars
-export CARGO_TERM_PROGRESS_WHEN="${BROWSER_TV_CARGO_PROGRESS_BAR}"
-export CARGO_TERM_PROGRESS_WIDTH=80
+readonly CARGO_TERM_PROGRESS_WHEN="${BROWSER_TV_CARGO_PROGRESS_BAR}"
+readonly CARGO_TERM_PROGRESS_WIDTH=80
+export CARGO_TERM_PROGRESS_WHEN
+export CARGO_TERM_PROGRESS_WIDTH
 
 ## Enable certificate revocation checks
-export CARGO_HTTP_CHECK_REVOKE=true
+readonly CARGO_HTTP_CHECK_REVOKE='true'
+export CARGO_HTTP_CHECK_REVOKE
 
 ## Enable colored output
-export CARGO_TERM_COLOR="${BROWSER_TV_CARGO_COLORED_OUTPUT}"
+readonly CARGO_TERM_COLOR="${BROWSER_TV_CARGO_COLORED_OUTPUT}"
+export CARGO_TERM_COLOR
 
 ## Enable HTTP2 multiplexing
 ### https://doc.rust-lang.org/cargo/reference/config.html#httpmultiplexing
-export CARGO_HTTP_MULTIPLEXING=true
+readonly CARGO_HTTP_MULTIPLEXING='true'
+export CARGO_HTTP_MULTIPLEXING
 
 ## Enable overflow checks
-export CARGO_PROFILE_DEV_OVERFLOW_CHECKS=true
-export CARGO_PROFILE_RELEASE_OVERFLOW_CHECKS=true
+readonly CARGO_PROFILE_DEV_OVERFLOW_CHECKS='true'
+readonly CARGO_PROFILE_RELEASE_OVERFLOW_CHECKS='true'
+export CARGO_PROFILE_DEV_OVERFLOW_CHECKS
+export CARGO_PROFILE_RELEASE_OVERFLOW_CHECKS
 
 ## Enable performance optimizations
-export CARGO_PROFILE_DEV_LTO=true
-export CARGO_PROFILE_DEV_OPT_LEVEL=3
-export CARGO_PROFILE_RELEASE_LTO=true
-export CARGO_PROFILE_RELEASE_OPT_LEVEL=3
-
-## Remove user agent
-### https://doc.rust-lang.org/cargo/reference/config.html#httpuser-agent
-export CARGO_HTTP_USER_AGENT=''
+readonly CARGO_PROFILE_DEV_LTO='true'
+readonly CARGO_PROFILE_DEV_OPT_LEVEL=3
+readonly CARGO_PROFILE_RELEASE_LTO='true'
+readonly CARGO_PROFILE_RELEASE_OPT_LEVEL=3
+export CARGO_PROFILE_DEV_LTO
+export CARGO_PROFILE_DEV_OPT_LEVEL
+export CARGO_PROFILE_RELEASE_LTO
+export CARGO_PROFILE_RELEASE_OPT_LEVEL
 
 ## Strip debug info
-export CARGO_PROFILE_DEV_STRIP='debuginfo'
-export CARGO_PROFILE_RELEASE_STRIP='debuginfo'
+readonly CARGO_PROFILE_DEV_STRIP='debuginfo'
+readonly CARGO_PROFILE_RELEASE_STRIP='debuginfo'
+export CARGO_PROFILE_DEV_STRIP
+export CARGO_PROFILE_RELEASE_STRIP
 
 # rustup
-export RUSTUP_HOME="${BROWSER_TV_RUSTUP_HOME}"
+readonly RUSTUP_HOME="${BROWSER_TV_RUSTUP_HOME}"
+export RUSTUP_HOME
 
 ## Display progress bars
-export RUSTUP_TERM_PROGRESS_WHEN="${BROWSER_TV_RUSTUP_PROGRESS_BAR}"
+readonly RUSTUP_TERM_PROGRESS_WHEN="${BROWSER_TV_RUSTUP_PROGRESS_BAR}"
+export RUSTUP_TERM_PROGRESS_WHEN
 
 ## Enable colored output
-export RUSTUP_TERM_COLOR="${BROWSER_TV_RUSTUP_COLORED_OUTPUT}"
+readonly RUSTUP_TERM_COLOR="${BROWSER_TV_RUSTUP_COLORED_OUTPUT}"
+export RUSTUP_TERM_COLOR
 
 # Include version info
 source "${BROWSER_TV_VERSIONS}"
 
 ## Set Rust version
-export RUSTUP_TOOLCHAIN="${RUST_VERSION}"
+readonly RUSTUP_TOOLCHAIN="${RUST_VERSION}"
+export RUSTUP_TOOLCHAIN
 
 ## Set Rustup version
 export RUSTUP_VERSION
